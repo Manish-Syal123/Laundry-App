29f8fc3f82df562c8bd49f30cb7c3f6d53c2947d
Manish_Syal <manishsyal77@gmail.com>
2023-07-23 23:19:36 +0530

    Changed Lottie animation and designed Next & Done button

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -11,6 +11,7 @@ import Onboarding from "react-native-onboarding-swiper";
 import Lottie from "lottie-react-native";
 import { useNavigation } from "@react-navigation/native";
 import { setItem } from "../utils/asyncStorage";
+import { AntDesign, Feather, FontAwesome5, Ionicons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
 
@@ -24,8 +25,23 @@ const OnboardingScreen = () => {
 
   const doneButton = ({ ...props }) => {
     return (
-      <TouchableOpacity style={styles.doneButton} {...props}>
-        <Text>Done</Text>
+      <TouchableOpacity style={styles.nextButton} {...props}>
+        <Text style={{ color: "white", fontSize: 17 }}>Done</Text>
+        <Ionicons
+          name="ios-checkmark-done-circle-sharp"
+          size={29}
+          color="white"
+        />
+      </TouchableOpacity>
+    );
+  };
+  const nextButton = ({ ...props }) => {
+    return (
+      <TouchableOpacity style={styles.nextButton} {...props}>
+        <Text style={{ color: "white", fontSize: 17 }}>Next</Text>
+        {/* <AntDesign name="arrowright" size={24} color="white" /> */}
+        {/* <Feather name="arrow-right-circle" size={24} color="white" /> */}
+        <FontAwesome5 name="arrow-circle-right" size={24} color="white" />
       </TouchableOpacity>
     );
   };
@@ -35,51 +51,74 @@ const OnboardingScreen = () => {
         onDone={handleDone}
         onSkip={handleDone}
         DoneButtonComponent={doneButton}
+        //SkipButtonComponent={skipButton}
+        NextButtonComponent={nextButton}
         bottomBarHighlight={false}
         containerStyles={{ paddingHorizontal: 15 }}
         pages={[
           {
-            backgroundColor: "#a7f3d0",
+            //backgroundColor: "#a78bfa",
+            // backgroundColor: "#35363A", //light black
+            backgroundColor: "#444654", //light black + lavender
+
             image: (
               <View style={styles.lottie}>
                 <Lottie
-                  source={require("../assets/animation/boost.json")}
-                  autoPlay
-                  loop
-                />
-              </View>
-            ),
-            title: "Boost Productivity",
-            subtitle: "Subscribe this channel to boost your productivity level",
-          },
-          {
-            backgroundColor: "#fef3c7",
-            image: (
-              <View style={styles.lottie}>
-                <Lottie
+                  //source={require("../assets/animation/boost.json")}
                   source={require("../assets/animation/work.json")}
                   autoPlay
                   loop
                 />
               </View>
             ),
-            title: "Work Seamlessly",
-            subtitle: "Get your work done seamlessly without interruption",
+            title: "Laundry in Devices",
+            subtitle:
+              "Now you can access laundry service Under your fingure tip ",
           },
           {
-            backgroundColor: "#a78bfa",
+            // backgroundColor: "#fef3c7",
+            // backgroundColor: "#E6E6FA", // light lavender
+            backgroundColor: "#66cdaa", // green
             image: (
               <View style={styles.lottie}>
                 <Lottie
-                  source={require("../assets/animation/achieve.json")}
+                  source={require("../assets/animation/washingMachine1.json")}
                   autoPlay
                   loop
                 />
               </View>
             ),
-            title: "Achieve Higher Goals",
+            title: "Highly advanced machines available",
+            subtitle: "Large number of machines with self drying cloths in it",
+          },
+          {
+            backgroundColor: "#E6E6FA",
+            image: (
+              <View style={styles.lottie}>
+                <Lottie
+                  source={require("../assets/animation/ironingCloths.json")}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: "Ironing Cloths",
             subtitle:
-              "By boosting your productivity we help you to achieve higher goals",
+              "Your cloths are ironed just before arriving for delevery.",
+          },
+          {
+            backgroundColor: "#ded7c1",
+            image: (
+              <View style={styles.lottie}>
+                <Lottie
+                  source={require("../assets/animation/HomeCleaning.json")}
+                  autoPlay
+                  loop
+                />
+              </View>
+            ),
+            title: "Home Cleaning",
+            subtitle: "Sooner we will add Home cleaning service as well......",
           },
         ]}
       />
@@ -103,5 +142,17 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderTopLeftRadius: 100,
     borderBottomLeftRadius: 100,
+  },
+  nextButton: {
+    backgroundColor: "#121212",
+    padding: 11,
+    width: 113,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginRight: 5,
+    borderWidth: 2,
+    borderColor: "white",
   },
 });
